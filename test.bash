6a075345ab70cb894c03a0a3bace23accf74f362
Kazuki Fujita <kazu.maro.126624@icloud.com>
2025-11-06 11:07:39 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Kazuki Fujita
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよよよよ
+	echo ${1}行目が違うよ
 	res=1
 }
 
@@ -9,6 +11,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
