2f795ee222fa1cf0cbe5db47d34649adf00d1d79
Kazuki Fujita <kazu.maro.126624@icloud.com>
2025-12-24 19:12:43 +0900

    add test

--- test.bash
+++ test.bash
@@ -3,22 +3,37 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
-
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+# 正常系
+out=$(echo 2 | ./factorize)
+[ "$out" = "2" ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo 12 | ./factorize)
+[ "$out" = "2*2*3" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo 36 | ./factorize)
+[ "$out" = "2*2*3*3" ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+# 素数
+out=$(echo 97 | ./factorize)
+[ "$out" = "97" ] || ng "$LINENO"
+
+# 境界値
+out=$(echo 1 | ./factorize)
+[ "$out" = "1" ] || ng "$LINENO"
+
+# 異常入力
+out=$(echo a | ./factorize 2>/dev/null)
+[ "$?" = 1 ] || ng "$LINENO"
+
+# 空入力
+out=$(echo | ./factorize 2>/dev/null)
+[ "$?" = 1 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo "OK"
 exit $res
+
